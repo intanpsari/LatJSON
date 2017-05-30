c9a5d60295e19f352d2a99712bb2515d0eaa1ba0
Intan Permatasari <Intan Permatasari@DESKTOP-N1FKJCQ>
2017-05-30 09:11:46 +0700

    update2

---
+++ LatJSON/LatJSON/GameResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatJSON
+{
+    class GameResult
+    {
+        public DateTime GameDate { get; set; }
+        public string TeamName { get; set; }
+        public HomeOrAway HomeOrAway { get; set; }
+        public int Goals { get; set; }
+
+        public int GoalAttempts { get; set; }
+
+        public int ShotsOnGoal { get; set; }
+
+        public int ShotsOffGoal { get; set; }
+        public Double PossessionPercent { get; set; }
+
+        public Double ConversionRate
+        {
+            get
+            {
+                return (double)Goals / (double)GoalAttempts;
+            }
+        }
+    }
+
+    public enum HomeOrAway //buat tipe data baru HomeOrAway
+    {
+        Home,
+        Away
+    }
+}
---
+++ LatJSON/LatJSON/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace LatJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory(); //call the path
+            
+            var fileName = Path.Combine(currentDirectory, "players.json");
+            
+            var fileContents = DeserializedPlayer(fileName);
+
+           foreach (var line in fileContents)
+           Console.WriteLine(line.FirstName);
+
+        }
+        
+        public static List<GameResult> ReadFootballResults(string fileName)
+        {
+            var soccerResult = new List<GameResult>();
+            using (var reader = new StreamReader(fileName))
+            {
+                var line = "";
+                reader.ReadLine();
+                while((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+           
+                    GameResult gr = new GameResult();
+                    DateTime gameDate;
+                    HomeOrAway homeOrAway;
+
+                    if (DateTime.TryParse(values[0], out gameDate))
+                        gr.GameDate = gameDate;
+
+                    gr.TeamName = values[1];
+
+                    if (Enum.TryParse(values[2], out homeOrAway))
+                        gr.HomeOrAway = homeOrAway;
+
+                    int parseInt;
+                    if (int.TryParse(values[3], out parseInt))
+                    {
+                        gr.Goals = parseInt;
+                    }
+                    if (int.TryParse(values[4], out parseInt))
+                    {
+                        gr.GoalAttempts = parseInt;
+                    }
+                    if (int.TryParse(values[5], out parseInt))
+                    {
+                        gr.ShotsOnGoal = parseInt;
+                    }
+                    if (int.TryParse(values[6], out parseInt))
+                    {
+                        gr.ShotsOffGoal = parseInt;
+                    }
+
+                    Double PossessionPercent;
+                    if (Double.TryParse(values[7], out PossessionPercent))
+                    {
+                        gr.PossessionPercent = PossessionPercent;
+                    }
+
+                    soccerResult.Add(gr);
+
+                }
+            }
+            return soccerResult;
+        }
+
+        public static List<Player> DeserializedPlayer(string fileName)
+        {
+            var players = new List<Player>();
+            var serializer = new JsonSerializer();
+
+            using (var reader = new StreamReader(fileName))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                players = serializer.Deserialize<List<Player>>(jsonReader);
+            }
+
+            return players;
+        }
+
+    }
+
+
+}
